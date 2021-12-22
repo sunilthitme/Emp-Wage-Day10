428ed50f268038f210730d1e14316618c91cd98e
sunilthitme <thitamesunil808@gmail.com>
2021-12-22 13:39:17 +0530

    Presenty

---
+++ EmpWage.java
@@ -0,0 +1,15 @@
+package com.empwageday10;
+
+public class EmpWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation program !! ");
+		int is_present = 1;
+		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
+		if( emp_check == is_present)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+			System.out.println("Employee is absent");
+	}
+}
